60da63742892b2a1bb57b573349bb37b6091195c
alayor <alayor3@gmail.com>
2018-09-18 13:28:03 -0600

    Get orders from ordering service.

--- src/services/order_service/index.js
+++ src/services/order_service/index.js
@@ -1,11 +1,9 @@
+const ListOrdersRequest = require('src/services/requests/ordering-service/list_orders')
+const apiRequester = require('src/services/api_requester')
+
 const list = async ctx => {
-  return [{
-    id: 1,
-    jamon: 0,
-    lomo: 2,
-    especial: 0,
-    refresco: 1
-  }]
+  const request = new ListOrdersRequest(ctx.request.body)
+  ctx.body = await apiRequester.send(request, ctx.request.headers['is-test'])
 }
 
 module.exports = {
