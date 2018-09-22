e672371b9f48140c501821a7b38030b3dba63cb4
alayor <alayor3@gmail.com>
2018-09-22 00:52:33 -0600

    placed order socket

--- app/index.js
+++ app/index.js
@@ -2,6 +2,7 @@ const HttpServer = require('setup/server/http')
 const SocketServer = require('setup/server/socket')
 const orderWebEndpoints = require('src/api/http_endpoints/order_endpoints')
 const orderPlacementChannel = require('src/api/socket_channels/order_placement')
+const OrderingSocketClient = require('setup/client/socket/ordering_socket_client')
 
 const httpServer = new HttpServer([
   ...orderWebEndpoints
@@ -10,5 +11,7 @@ const httpServer = new HttpServer([
 const socketServer = new SocketServer(httpServer.server, [
   ...orderPlacementChannel
 ])
+
+new OrderingSocketClient(socketServer).start()
 
 httpServer.start(socketServer)
--- setup/client/socket/ordering_socket_client/index.js
+++ setup/client/socket/ordering_socket_client/index.js
@@ -1,6 +1,6 @@
 const io = require('socket.io-client')
 const config = require('config')
-const redis = require('services/redis')
+const redis = require('setup/redis')
 
 class OrderingSocketClient {
   constructor(socketServer) {
@@ -9,15 +9,18 @@ class OrderingSocketClient {
 
   start() {
     const socketClient = io(config.ordering_service_host)
-    socketClient.on('placed_order', async data => {
-      const socketIds = await redis.georadius(
+    socketClient.on('placed_order', async response => {
+      const redisClient = redis.getRedisClient(response.meta.isTest)
+      const socketIds = await redisClient.georadius(
         'kitchen_sockets_for_order_placements',
-        data.customerLocation.longitude,
-        data.customerLocation.latitude,
+        response.data.customerLocationLongitude,
+        response.data.customerLocationLatitude,
+        '20',
+        'km'
       )
       const socketId = socketIds[0]
       if (Object.keys(this.socketServer.sockets.server.eio.clients).includes(socketId)) {
-        await this.socketServer.to(socketId).emit('placed_order', data)
+        await this.socketServer.to(socketId).emit('placed_order', response.data)
       } else if (socketId) {
         await this.socketServer.emit('warning', `Socket ${socketId} is not connected anymore.`)
       }
