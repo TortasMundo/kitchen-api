0c501e0881813ed10ea11819aaa6f1ca51deca72
alayor <alayor3@gmail.com>
2018-09-23 11:16:14 -0600

    Remove knex.

--- config.js
+++ config.js
@@ -1,11 +1,5 @@
-const KnexFile = require('knexfile')
-const Knex = require('knex')
-const testKnex = Knex(KnexFile['test'])
-const devKnex = Knex(KnexFile['development'])
-
 module.exports = {
   port: process.env.PORT || 4000,
   redis_host: process.env.REDISCLOUD_URL || 'caching:6379',
   ordering_service_host: `${process.env.ORDERING_SERVICE_URL || 'http://ordering-service:4002'}`,
-  getKnex: isTest => (isTest ? testKnex : devKnex)
 }
