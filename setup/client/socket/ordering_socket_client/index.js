091c94b6e18df705538de9930037c2c96be84ae9
alayor <alayor3@gmail.com>
2018-09-25 22:15:53 -0600

    Fix socket client.

--- setup/client/socket/ordering_socket_client/index.js
+++ setup/client/socket/ordering_socket_client/index.js
@@ -10,21 +10,27 @@ class OrderingSocketClient {
   start() {
     const socketClient = io(config.ordering_service_host)
     socketClient.on('placed_order', async response => {
-      const redisClient = redis.getRedisClient(response.meta.isTest)
-      const socketIds = await redisClient.georadius(
-        'kitchen_sockets_for_order_placements',
-        response.data.customerLocationLongitude,
-        response.data.customerLocationLatitude,
-        '200',
-        'km'
-      )
-      const socketId = socketIds[0]
+      try {
+        const redisClient = redis.getRedisClient(response.meta.isTest)
+        const socketIds = await redisClient.georadius(
+          'kitchen_sockets_for_order_placements',
+          response.data.customer_location_longitude,
+          response.data.customer_location_latitude,
+          '200',
+          'km'
+        )
+        const socketId = socketIds[0]
 
-      if (Object.keys(this.socketServer.sockets.server.eio.clients).includes(socketId)) {
-        await this.socketServer.to(socketId).emit('placed_order', response.data)
-      } else if (socketId) {
-        await this.socketServer.emit('warning', `Socket ${socketId} is not connected anymore.`)
+        if (Object.keys(this.socketServer.sockets.server.eio.clients).includes(socketId)) {
+          await this.socketServer.to(socketId).emit('placed_order', response.data)
+        } else if (socketId) {
+          await this.socketServer.emit('warning', `Socket ${socketId} is not connected anymore.`)
+        }
+      } catch (e) {
+        console.log('e: ', e)
+        await this.socketServer.emit('error', e)
       }
+
     })
   }
 }
