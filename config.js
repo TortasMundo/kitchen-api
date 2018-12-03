3f829d9c66d8a3773ea2516732fe0472dd2ec39d
Alonso A. Ortega <alayor3@gmail.com>
2018-12-02 20:58:45 -0700

    Modify ports

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   port: process.env.PORT || 5000,
   redis_host: process.env.REDISCLOUD_URL || 'caching:6379',
-  ordering_service_host: `${process.env.ORDERING_SERVICE_URL || 'http://ordering-service:4002'}`,
+  ordering_service_host: `${process.env.ORDERING_SERVICE_URL || 'http://ordering-service:5002'}`,
 }
